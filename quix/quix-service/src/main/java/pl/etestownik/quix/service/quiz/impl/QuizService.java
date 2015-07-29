62f68598d6486fbdeea419da0ea6296e69d2ab82
unknown <rkpieniazek@gmail.com>
2015-07-29 15:42:10 +0200

    QuizService

--- quix/quix-service/src/main/java/pl/etestownik/quix/service/quiz/impl/QuizService.java
+++ quix/quix-service/src/main/java/pl/etestownik/quix/service/quiz/impl/QuizService.java
@@ -4,27 +4,46 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import pl.etestownik.quix.model.answer.Answer;
+import pl.etestownik.quix.model.content.Content;
+import pl.etestownik.quix.model.question.Question;
 import pl.etestownik.quix.model.quiz.Quiz;
-import pl.etestownik.quix.repo.quiz.IQuizRepo;
+import pl.etestownik.quix.repo.base_repo.IBaseRepo;
 import pl.etestownik.quix.service.quiz.IQuizService;
 
 public class QuizService implements IQuizService{
 
 	@Autowired
-	private IQuizRepo quizRepo;
-
+	private IBaseRepo<Quiz> quizRepo;
+	
+	@Autowired
+	private IBaseRepo<Question> questionRepo;
+	
+	@Autowired
+	private IBaseRepo<Answer> anwerRepo;
+	
+	@Autowired
+	private IBaseRepo<Content> contentRepo;
+	
 	public void save(Quiz quiz) {
 		quizRepo.save(quiz);
-		
+	//	questionRepo.save(entity);??
 	}
 
 	public void delete(Quiz quiz) {
 		quizRepo.delete(quiz);
-		
 	}
 
 	public List<Quiz> findAll() {
 		return quizRepo.findAll();
+	}
+	
+	public void update(Quiz entity) {
+		quizRepo.update(entity);
+	}
+
+	public Quiz getById(long id) {
+		return quizRepo.getById(id);
 	}
 
 }
